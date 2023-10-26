154b73c719e313eb6e0f594ae2c02c701a67db36
PJ_Miller <pj.horus@gmail.com>
2023-10-26 11:55:00 +0200

    Update streamlit_app.py
    commented out the json response, Changed watermelon to kiwi API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advise!')
 
 # makes API request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # writes data on streamlit
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
