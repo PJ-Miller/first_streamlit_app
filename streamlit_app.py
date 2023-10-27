5958c64b48c35ce1a7a1c988fdf9de3e623b1511
PJ_Miller <pj.horus@gmail.com>
2023-10-27 07:05:11 +0200

    Update streamlit_app.py
    added try and except to check if the fruit exists.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -27,17 +28,26 @@ streamlit.dataframe(fruits_to_show)
 # new header
 streamlit.header('Fruityvice Fruit Advise!')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error()
+
+# streamlit.write('The user entered ', fruit_choice)
 
 # makes API request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # writes data on streamlit
 # streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+
 # https://firstappapp-efxxxxdckshlipczxc3xgx.streamlit.app/
 
 
