e6dd1c82f66c3ca0ffd9f203fe297b26d83bd6da
PJ_Miller <pj.horus@gmail.com>
2023-10-27 07:36:47 +0200

    Update streamlit_app.py
    added the forgotten return

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("Select * from fruit_load_list")
-        my_cur.fetchall()
+        return my_cur.fetchall()
 
 # Add button to load the fruit
 if streamlit.button('Get Fruit Load List'):
